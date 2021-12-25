36e2650f5943d112f43a6875cc99330673aee170
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2021-12-25 20:24:44 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,48 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          backgroundColor: Colors.red,
           title: Text('Dicee'),
         ),
-        body: DiceePage(),
+        body: DicePage(),
       ),
     );
   }
 }
 
-class DiceePage extends StatelessWidget {
-  const DiceePage({Key? key}) : super(key: key);
+class DicePage extends StatelessWidget {
+  int leftImageNumber = 3;
+  int rightImageNumber = 2;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Left button pressed!');
+              },
+              child: Image.asset('images/dice$leftImageNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Right button pressed!');
+              },
+              child: Image.asset('images/dice$rightImageNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
